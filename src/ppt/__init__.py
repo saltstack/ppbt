f10128dbb19abc2d9cc6090b78514ab89dbdd4a7
Daniel A. Wozniak <daniel.wozniak@broadcom.com>
2024-08-31 01:26:42 -0700

    Clean up top level of package

--- src/ppt/__init__.py
+++ src/ppt/__init__.py
@@ -1,5 +1,8 @@
-# Copyright 2024 VMware, Inc.
+# Copyright 2024 Broadcom Corporation
 # SPDX-License-Identifier: Apache-2.0
 """
 Protable toolchains for python
 """
+from environ import environ
+
+ALL = ("environ",)
